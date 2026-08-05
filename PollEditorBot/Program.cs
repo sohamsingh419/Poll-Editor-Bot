7695f6eea4e93b83f2db1a4ac2d8372f6736c544
sohamsingh419 <skhackeriam@gmail.com>
2026-08-05 16:30:19 +0530

    Refactor Program.cs to use Telegram Bot API

--- PollEditorBot/Program.cs
+++ PollEditorBot/Program.cs
@@ -1,41 +1,31 @@
 using System;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Extensions;
+using Telegram.Bot.Polling;
+using PollEditorBot.Extensions;
 using PollEditorBot;
 using PollEditorBot.Loggers;
-
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-app.MapGet("/", () => "Poll Editor Bot is running!");
-
-_ = app.RunAsync();
 
 ILogger logger = new ConsoleLogger();
 PollEditorBotLauncher bot = new(logger);
 
 CancellationTokenSource cts = new();
 
-Console.CancelKeyPress += (_, e) =>
+Console.CancelKeyPress += (, e) =>
 {
-    e.Cancel = true;
-    cts.Cancel();
+e.Cancel = true;
+cts.Cancel();
 };
-
-AppDomain.CurrentDomain.ProcessExit += (_, _) => cts.Cancel();
+AppDomain.CurrentDomain.ProcessExit += (, _) => cts.Cancel();
 
 await bot.StartReceivingAsync(cts);
 
 logger.LogInformationLine("Bot is running. Press Ctrl+C to stop.");
 
-try
-{
-    await Task.Delay(Timeout.Infinite, cts.Token);
-}
-catch (OperationCanceledException)
-{
-}
+try { await Task.Delay(Timeout.Infinite, cts.Token); }
+catch (OperationCanceledException) { }
